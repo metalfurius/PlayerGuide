9ae9c716c6dde6c402b7c084c2d17ab3fe150c26
metalfurius <75508084+metalfurius@users.noreply.github.com>
2025-11-17 15:10:19 +0100

    Create 5.cs

---
+++ 5.cs
@@ -0,0 +1,36 @@
+// Name all types of variables in C# and print their values
+int a = 1;
+uint b = 2;
+
+short c = 3;
+ushort d = 4;
+
+long e = 5;
+ulong f = 6;
+
+byte g = 7;
+sbyte h = 8;
+
+char i = '9';
+string j = "10";
+
+bool k = true;
+
+float l = 11.0f;
+double m = 12.0;
+decimal n = 13.0m;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
