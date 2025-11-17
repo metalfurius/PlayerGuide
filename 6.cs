55ad06a3d2947b525ccea6b1c48b556772a439fb
metalfurius <75508084+metalfurius@users.noreply.github.com>
2025-11-17 15:11:41 +0100

    Create 6.cs

---
+++ 6.cs
@@ -0,0 +1,51 @@
+// Modify values of all variable types in C# and print their new values
+int a = 1;
+uint b = 2;
+
+short c = 3;
+ushort d = 4;
+
+long e = 5;
+ulong f = 6;
+
+byte g = 7;
+sbyte h = 8;
+
+char i = '9';
+string j = "10";
+
+bool k = true;
+
+float l = 11.0f;
+double m = 12.0;
+decimal n = 13.0m;
+
+a = 10;
+b = 20;
+c = 30;
+d = 40;
+e = 50;
+f = 60;
+g = 70;
+h = 80;
+i = 'Z';
+j = "Modified";
+k = false;
+l = 110.0f;
+m = 120.0;
+n = 130.0m;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(f);
+Console.WriteLine(g);
+Console.WriteLine(h);
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
