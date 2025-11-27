76646fc8819156a9f4cf836f76f30c854dc12b40
metalfurius <75508084+metalfurius@users.noreply.github.com>
2025-11-27 11:23:57 +0100

    Refactor Point class and add Color class
    Renamed Point class fields to properties for better encapsulation and updated usage accordingly. Added a new Color class with RGB properties, predefined color instances, and a custom ToString method.

--- 028_PointClass.cs
+++ 028_PointClass.cs
@@ -3,14 +3,14 @@
 Point a = new(2, 3);
 Point b = new(-4, 0);
 
-Console.WriteLine($"Point A: ({a._x}, {a._y})");
-Console.WriteLine($"Point B: ({b._x}, {b._y})");
-Console.WriteLine($"Point C (default): ({new Point()._x}, {new Point()._y})");
+Console.WriteLine($"Point A: ({a.X}, {a.Y})");
+Console.WriteLine($"Point B: ({b.X}, {b.Y})");
+Console.WriteLine($"Point C (default): ({new Point().X}, {new Point().Y})");
 
 public class Point (int x, int y)
 {
-    public int _x = x;
-    public int _y = y;
+    public int X = x;
+    public int Y = y;
 
     public Point() :this(0, 0) { }
 }
---
+++ 029_ColorClass.cs
@@ -0,0 +1,24 @@
+Color x = new(222, 33, 44);
+Color white = Color.White;
+
+Console.WriteLine(x);
+Console.WriteLine(white);
+
+public class Color(int r, int g, int b)
+{
+    public int R { get; } = r;
+    public int G { get; } = g;
+    public int B { get; } = b;
+
+    public override string ToString() => $"Color(R: {R}, G: {G}, B: {B})";
+
+    public static Color Black => new(0, 0, 0);
+    public static Color White => new(255, 255, 255);
+    public static Color Red => new(255, 0, 0);
+    public static Color Yellow => new(255, 255, 0);
+    public static Color Cyan => new(0, 255, 255);
+    public static Color Magenta => new(255, 0, 255);
+    public static Color Green => new(0, 255, 0);
+    public static Color Blue => new(0, 0, 255);
+
+}
